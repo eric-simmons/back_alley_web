9781a8b874ecb58ae9e0eb91e1b93282c079bfe4
Eric Simmons <erictomlinsonsimmons@gmail.com>
2025-11-07 08:53:52 -0600

    render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
 bundle install
 yarn install
+yarn build:css
 bundle exec rake assets:precompile
 bundle exec rake db:migrate
