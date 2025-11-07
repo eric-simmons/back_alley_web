6c5165b98083147be296c0a1779d5217f4527a12
Eric Simmons <erictomlinsonsimmons@gmail.com>
2025-11-07 15:18:44 -0600

    render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
+mkdir -p tmp/cache
+export BUNDLE_PATH=vendor/bundle
+export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"
+
 
 bundle install
 yarn install
