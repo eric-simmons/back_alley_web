00d7f1f6c388642e9d1450114ce3c1bb4de39804
Eric Simmons <erictomlinsonsimmons@gmail.com>
2026-02-18 16:30:17 -0600

    latest

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "pages#home"
-  get "/cabs", to: "pages#cabs", as: :cabs
+  get "/cabs",     to: "pages#cabs",     as: :cabs
+  get "/cabs/:id", to: "pages#show_cab", as: :cab
 end
